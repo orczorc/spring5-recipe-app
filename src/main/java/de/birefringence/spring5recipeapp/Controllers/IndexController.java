06978b6935175db5acc78cdd712de7f1c5542480
tarrok <tarrok@tarrok.com>
2018-01-01 20:18:18 +0100

    First Commit

---
+++ src/main/java/de/birefringence/spring5recipeapp/Controllers/IndexController.java
@@ -0,0 +1,14 @@
+package de.birefringence.spring5recipeapp.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        System.out.println("Some message... Go Cows!yeah");
+        return "index";
+    }
+}
